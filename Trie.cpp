d6c6a516a583319537422cc54b7c59d29a25ec84
GwenaelleHorrocks <gwenaellehorrocks@gmail.com>
2024-10-12 22:15:37 -0600

    Refactored Trie.cpp
    Refactored addWord, isWord, and prefix methods to work with map implementation.

--- Trie.cpp
+++ Trie.cpp
@@ -39,85 +39,82 @@ void Trie::addWord(string word) {
     }
 
     // get the index of the first letter in word, set nextTrie equal to the trie in this trie's index
-    int index = word[0] - 'a';
-    Trie* nextTrie = this->dictionary[index];
+    char letter = word[0];
+    //Trie* nextTrie = this->dictionary[letter];
 
-    if (!nextTrie) {
-        nextTrie = new Trie();
-        this->dictionary[index] = nextTrie;
+    if (!(dictionary.contains(letter))) {
+        Trie newTrie;
+        dictionary[letter] = newTrie;
     }
 
     if (word.length() == 1) {
-        dictionary[index]->validWord = true;
+        dictionary[letter].validWord = true;
         return;
     }
 
     // recursively call addWord with word minus the first letter
-    nextTrie->addWord(word.substr(1));
+    dictionary[letter].addWord(word.substr(1));
 }
 
 bool Trie::isWord(string word) {
     // when word is empty, the end of the word has been reached and validWord is returned. Since empty strings are never
     // added, this will return false for an empty word.
     if (word.length() == 0) {
-        return this->validWord;
+        return validWord;
     }
 
     // get the index of the first letter in word, set nextTrie equal to the trie in this trie's index
-    int index = word[0] - 'a';
-    if (index < 0 || index > 25) {
+    char letter = word[0];
+    if (letter < 'a' || letter > 'z') {
         return false;
     }
-    Trie* nextTrie = this->dictionary[index];
 
-    if (!nextTrie) {
+    if (!(dictionary.contains(letter))) {
         return false;
     }
 
     // recursively call isWord with word minus the first letter
-    return nextTrie->isWord(word.substr(1));
+    return dictionary[letter].isWord(word.substr(1));
 }
 
 vector<string> Trie::allWordsStartingWithPrefix(string prefix) {
     vector<string> prefixWords;
-    Trie* currentTrie = this;
-    Trie* nextTrie;
+    Trie currentTrie = *this;
+    Trie nextTrie;
 
     // iteratively navigate down the trie to the end of the prefix
     for (size_t i = 0; i < prefix.length(); i++) {
-        int prefixIndex = prefix[i] - 'a';
-        nextTrie = currentTrie->dictionary[prefixIndex];
+        char letter = prefix[i];
+        //nextTrie = currentTrie->dictionary[letter];
 
-        if (prefixIndex < 0 || prefixIndex >= 26) {
+        if (letter < 'a' || letter >= 'z') {
             return prefixWords;
         }
 
-        if (!nextTrie) {
+        if (!(currentTrie.dictionary.contains(letter))) {
             return prefixWords;
         }
 
-        currentTrie = nextTrie;
+        currentTrie = dictionary[letter];
     }
 
     // at this point, currentTrie represents the node corresponding to the prefix
     // collect all words starting from currentTrie
-    currentTrie->prefixRecursive(prefix, prefixWords);
+    currentTrie.prefixRecursive(prefix, prefixWords);
 
     return prefixWords;
 }
 
 void Trie::prefixRecursive(string prefix, vector<string>& words) {
     // add current word to the vector if it is marked as a valid word
-    if (this->validWord) {
+    if (validWord) {
         words.push_back(prefix);
     }
 
     // depth-first recursion into all words that start with prefix, adding them to the vector if they are valid words
-    for (int i = 0; i < 26; i++) {
-        if (this->dictionary[i]) {
-            char currentLetter = 'a' + i;
-            string newPrefix = prefix + currentLetter;
-            this->dictionary[i]->prefixRecursive(newPrefix, words);
-        }
+    for (auto pair : dictionary) {
+        char currentLetter = pair.first;
+        string newPrefix = prefix + currentLetter;
+        dictionary[pair.first].prefixRecursive(newPrefix, words);
     }
 }
